1878dbaa680c422090bfe115546c63b36e97bd39
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-08 23:04:16 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ import { theme } from './theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider
+      theme={theme}
+    >
       <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
